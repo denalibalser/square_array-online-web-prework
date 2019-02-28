32eed88ae82f098a4e1c84b6df9c32a3e753aee9
Denali Balser <denali.balser167@gmail.com>
2019-02-28 19:08:30 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,5 @@
 def square_array(array)
   numbers = [1, 2, 3]
-  array.each {|x| puts x ** 2}
-  new_numbers = array.each
-  new_numbers
+  new_numbers = array.each {|x| puts x ** 2}
+  new_numbers 
 end
